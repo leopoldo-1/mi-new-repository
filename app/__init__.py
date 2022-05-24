6dda0679d99c1573c7005d9b363f65bf4d506b31
leopoldo-1 <leopoldo.miranda18@tectijuana.edu.mx>
2022-05-24 16:04:58 -0700

    Added products endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
   return "<h1>Hello World!</h1>"
+
+@app.route("/products")
+def products():
+  product_list = ["Apples", "Oranges", "Bannans"]
+
+  bullet_list = "".join("<li>%s</li>" % product for product in product_list)
+  return "<ul>%s</ul>" % bullet_list
